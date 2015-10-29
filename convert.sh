586340e835c6f6f9c65431e4a9020f27206b78c7
Jules R <jules.randolph@usherbrooke.ca>
2015-10-29 14:45:16 -0400

    Added avconv test

---
+++ convert.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+avconv -i 2015-01-30_11-15_EXP25_CAM_cuisiniere.mpeg -pix_fmt yuv420p -vcodec h264 -preset medium -crf 30 -crf_max 31 -r 12 -f mp4 2015-01-30_11-15_EXP25_CAM_cuisiniere.new.test.mp4
