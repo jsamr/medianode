97bf345a059a60f1adda94d2704524d67e33b3e1
ranj2004 <jules.randolph@usherbrooke.ca>
2015-11-13 14:10:18 -0500

    Erreur du nom de fichier redis conf

--- scripts/media-node.sh
+++ scripts/media-node.sh
@@ -2,7 +2,7 @@
 #Make path relative
 cd "${BASH_SOURCE%/*}" || (echo "FAILURE: impossible de trouver le répertoire courant" && exit 1)
 ## executer le script avec le groupe 'media-node'
-sg media-node -c "redis-server /etc/redis/media-node-redis.conf" &
+sg media-node -c "redis-server /etc/redis/redis.conf" &
 redisPID=$!
 echo "Redis lancé avec pid ${redisPID}"
 sg media-node -c "coffee ../src/app.coffee"
