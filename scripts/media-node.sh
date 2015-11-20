deb71ad5b08c9c4d8677d6c26696cd4f0ac8dba2
Jules R <jules.randolph@usherbrooke.ca>
2015-11-20 16:51:56 -0500

    check super utilisateur

--- scripts/media-node.sh
+++ scripts/media-node.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #Make path relative
 cd "${BASH_SOURCE%/*}" || (echo "FAILURE: impossible de trouver le répertoire courant" && exit 1)
+if [[ $(id -u) -ne 0 ]]; then
+    echo "FAILURE : Doit être éxécuté avec les privilèges super-utilisateur!" ; exit 1
+fi
 ## executer le script avec le groupe 'media-node'
 sg media-node -c "redis-server /etc/redis/redis-media-node.conf" &
 echo $?
